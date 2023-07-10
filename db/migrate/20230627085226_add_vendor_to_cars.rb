beba7d15dc0f67f7b75b99ccff53cf9b0468f3d9
Junaid-ahmed58 <junaid.ahmed@xprolabs.com>
2023-07-10 16:32:15 +0500

    Change datatype of some attributes

--- db/migrate/20230627085226_add_vendor_to_cars.rb
+++ db/migrate/20230627085226_add_vendor_to_cars.rb
@@ -2,9 +2,9 @@ class AddVendorToCars < ActiveRecord::Migration[7.0]
   def change
     add_column :cars, :vendor, :string
     add_column :cars, :car_name, :string
-    add_column :cars, :millage, :string
+    add_column :cars, :millage, :integer
     add_column :cars, :category, :string
     add_column :cars, :description, :string
-    add_column :cars, :engine_capacity, :string
+    add_column :cars, :engine_capacity, :integer
   end
 end
