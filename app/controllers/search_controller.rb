8173695d7a5e9d81d07858e42a2ab3329588c011
Junaid-ahmed58 <junaid.ahmed@xprolabs.com>
2023-07-13 15:21:46 +0500

    Add queries to model

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,14 +1,10 @@
 class SearchController < ApplicationController
   def search
-    @results = Car.all
-    @results = @results.where("vendor like ?", params[:vendor]) if params[:vendor].present?
-    @results = @results.where("car_name like ?", params[:car_name]) if params[:car_name].present?
-    @results = @results.where("model like ?", params[:model]) if params[:model].present?
-    @results = @results.where("category like ?", params[:category]) if params[:category].present?
-    @results = @results.where("engine_capacity like ?", params[:engine_capacity]) if params[:engine_capacity].present?
+    @results = Car.search(params)
     render turbo_stream:
-        turbo_stream.update('cars',
-                            partial: 'cars/cars',
-                            locals: { cars: @results})
+      turbo_stream.update('cars',
+                          partial: 'cars/cars',
+                          locals: { cars: @results })
   end
+  
 end
--- app/models/car.rb
+++ app/models/car.rb
@@ -3,4 +3,14 @@ class Car < ApplicationRecord
   VENDOR_NAMES = ['Toyota', 'Honda', 'Suzuki', 'Tesla', 'Mercedes', 'BMW', 'Hyundai', 'Kia'].freeze
   has_many_attached :images
   validates :vendor, :car_name, :model, :engine_capacity, :millage, :color, :price, presence: true
+
+  def self.search(params)
+    results = all
+    results = results.where("vendor LIKE ?", "%#{params[:vendor]}%") if params[:vendor].present?
+    results = results.where("car_name LIKE ?", "%#{params[:car_name]}%") if params[:car_name].present?
+    results = results.where("model LIKE ?", "%#{params[:model]}%") if params[:model].present?
+    results = results.where("category LIKE ?", "%#{params[:category]}%") if params[:category].present?
+    results = results.where("engine_capacity LIKE ?", "%#{params[:engine_capacity]}%") if params[:engine_capacity].present?
+    results
+  end
 end
