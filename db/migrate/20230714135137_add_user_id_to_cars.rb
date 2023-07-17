d7db72a837d2fc4e5556bbb7c8ce9eac5ff6befb
Junaid-ahmed58 <junaid.ahmed@xprolabs.com>
2023-07-17 14:24:46 +0500

    Integrate feedback

--- db/migrate/20230714135137_add_user_id_to_cars.rb
+++ db/migrate/20230714135137_add_user_id_to_cars.rb
@@ -1,6 +1,6 @@
 class AddUserIdToCars < ActiveRecord::Migration[7.0]
   def change
     add_column :cars, :user_id, :integer
-    add_index :cars, :user_id
+    add_foreign_key :cars, :users
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -52,7 +52,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_14_135137) do
     t.string "description"
     t.integer "engine_capacity"
     t.integer "user_id"
-    t.index ["user_id"], name: "index_cars_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -69,4 +68,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_14_135137) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "cars", "users"
 end
