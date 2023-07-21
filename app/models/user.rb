2933b2c89667c3d60d15b41790bf5100f6d03a6f
Junaid-ahmed58 <junaid.ahmed@xprolabs.com>
2023-07-21 12:10:09 +0500

    Add Enum in user to diff user and admin

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,9 @@
 class User < ApplicationRecord
-  
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  enum role: { user: 'user', admin: 'admin' }
+  enum status: { active: 'active', deactive: 'deactive' }
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
+  
   has_many :cars, dependent: :destroy
   has_many :likes, dependent: :destroy
   has_many :comments, dependent: :destroy
