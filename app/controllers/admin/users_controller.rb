2cdb4d10c1326a36b1dac2f288102b9ccc176eb5
Junaid-ahmed58 <junaid.ahmed@xprolabs.com>
2023-07-24 15:36:29 +0500

    Admin can delete any user

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,4 +1,6 @@
 class Admin::UsersController < AdminController
+  before_action :set_admin_user, only: [:toggle_status , :destroy]
+
   def index
     @users = User.all
   end
@@ -6,12 +8,24 @@ class Admin::UsersController < AdminController
   def show; end
 
   def toggle_status
-    @user = User.find(params[:id])
     @user.status = @user.active? ? 'inactive' : 'active'
     if @user.save
       redirect_to admin_users_path(@user), notice: 'User status has been updated.'
     else
       redirect_to admin_users_path(@user), alert: 'Failed to update user status.'
     end
+  end
+
+  def destroy
+    @user.destroy
+    respond_to do |format|
+      format.html { redirect_to admin_users_path(@users), notice: "User was deleted sucessfully" }
+    end
+  end
+
+  private
+
+  def set_admin_user
+    @user = User.find(params[:id])
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,1 @@
-User.create(email: 'admin@pakwheelsclone.com', password: '787898', password_confirmation: '787898', role: 'admin')
+User.create(email: 'admin@pakwheelsclone.com', password: '787898', password_confirmation: '787898', role: 'admin') if User.find_by(email: 'admin@pakwheelsclone.com').nil?
